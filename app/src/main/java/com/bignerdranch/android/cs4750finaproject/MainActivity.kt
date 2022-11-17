6346c3bbb7a15f427ccc658ad9895819dc241363
Robert Toribio <roberttoribio04@gmail.com>
2022-11-16 20:32:06 -0800

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/cs4750finaproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.cs4750finaproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

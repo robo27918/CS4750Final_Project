d03fb9676d5ce1ccf79f789b06a9864c885e3d5b
Robert Toribio <roberttoribio04@gmail.com>
2022-12-03 20:16:42 -0800

    Finished WordleViewModel.kt    Hardcoded wordle value for now -- need to change later

--- app/src/main/java/com/bignerdranch/android/cs4750finaproject/WordleViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/cs4750finaproject/WordleViewModel.kt
@@ -18,6 +18,7 @@ val DEFAULT_LETTER = Letter("", R.drawable.border, R.color.black)
 class WordleViewModel(): ViewModel(){
     val currPos = Position (0,0) //the game starts off at the left top corner (0,0)
     var guess: String = ""
+    var wordle = "ouija"
     /***
      *  How is this used?:
      *      Provides functions to emit values to the flow'
@@ -130,5 +131,59 @@ class WordleViewModel(): ViewModel(){
         }
     }
 
+    /***
+     * this function is used to compare the users guess
+     * to the actual wordle and then determine what color the
+     * Letter should be in the View -- however the actual view isnt adjusted here
+     * but in the function that follows
+     */
+    fun checkLetterColor ():List<Letter>{
+        val list = mutableListOf<Letter>()
+        listOfTextViews[currPos.row].forEachIndexed { index, flow ->
+            list.add(when (guess[index]) {
+                wordle[index] -> {
+                    Letter(flow.value.letter, R.color.green, R.color.white)
+                }
+                in wordle.filterIndexed { i, s -> guess[i] != s } -> {
+                    Letter(flow.value.letter, R.color.yellow, R.color.white)
+                }
+                else -> {
+                    Letter(flow.value.letter, R.color.dark_gray, R.color.white)
+                }
+            })
+        }
+        return list
+    }
 
+    /***
+     * This function changes the view according to the results of the function above
+     */
+    fun emitColor(list: List<Letter> = checkLetterColor()) {
+        viewModelScope.launch {
+            listOfTextViews[currPos.row].forEachIndexed { index, flow ->
+                val letter = list[index]
+                flow.emit(letter)
+                letterNotInWord.push(flow)
+
+                val key = listOfKeys[letter.letter]!!
+                val backgrd = when (key.value.backgrdColor) {
+                    R.color.green -> R.color.green
+                    R.color.yellow -> if (letter.backgrdColor == R.color.green) R.color.green else R.color.yellow
+                    else -> letter.backgrdColor
+                }
+
+                key.emit(Key(backgrd, letter.textColor))
+                keyNotInWord.push(key)
+            }
+        }
+    }
+
+    /***
+     * implememt fully once the list of differnt wordles is implemented.
+     */
+    fun getNewWord ()
+    {
+        wordle = "vowel"
+    }
 }
+
